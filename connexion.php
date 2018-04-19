26e679b49a4e75bf40abfe063cdf7607b21ddf87
ejuge <ejuge@A3401A>
2018-04-19 18:34:31 +0200

    connexion => clean

--- connexion.php
+++ connexion.php
@@ -1,91 +1,72 @@
 <?php
-session_start(); //session starts here
 
 function logged_only() {
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
     if (isset($_SESSION['auth'])) {
         if ($_SESSION['auth'] == 'admin') {
             header('Location: admin/admin_index.php');
             exit();
-        } 
+        }
     }
 }
-    logged_only();
-    if(isset($_REQUEST['id'])){    
-    
-    $id=$_REQUEST['id'];
-            }
-    ?>
 
+logged_only();
+if (isset($_REQUEST['id'])) {
+
+    $id = $_REQUEST['id'];
+
+    include 'header.php';
+}
+?>
 <html>
-    <head>
-        <title>LAFLEUR | ADMIN</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="keywords" content="LAFLEUR E-COMMERCE" />
-        <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-        <link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
-        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        <!-- Custom Theme files -->
-        <link href="css/style.css" rel='stylesheet' type='text/css' />
-        <!-- Custom Theme files -->
-        <!--webfont-->
-        <link href='//fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
-        <link href='//fonts.googleapis.com/css?family=Dorsa' rel='stylesheet' type='text/css'>
-        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
-        <!-- start menu -->
-        <link href="css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
-        <script src="js/jquery.easydropdown.js"></script>
-        <link rel="shortcut icon" href="images/favicon.ico" type="images/x-icon" />
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-        <link rel="shortcut icon" href="images/favicon.ico" type="images/x-icon" /> 
-    </head>
     <body>
         <div class="container">
-            <div class="row">
-                <div class="col-md-5 col-md-offset-3" style=" margin-top: 200">
-                    <div class="login-panel panel panel-success">
-                        <div class="panel-heading">
-                            <h3 class="panel-title">Connexion LAFLEUR</h3>
-                        </div>
-                        <?php 
-                        if(isset($_REQUEST['id'])){
-                           ?>
-                        <div class="panel-body col-md-11">
-                            <form role="form" method="POST" action="login.php">
-                                <fieldset>
-                                    <div class="form-group">
-                                        <input class="form-control" placeholder="Nom d'utilisateur" name="username" autofocus="">
+            <div class="row" style="margin-right: 150px; margin-left: 150px;">
+                <div class="box">
+                    <div class="info">
+                        <div class="card">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-offset-2 col-md-5">
+                                        <div class="login-panel">
+                                            <h1 align="center">Connexion LAFLEUR</h1>
+                                            <?php
+                                            if (isset($_REQUEST['id'])) {
+                                                ?>
+                                                <div class="panel-body col-md-11">
+                                                    <form role="form" method="POST" action="login.php">
+                                                        <fieldset>
+                                                            <div class="form-group">
+                                                                <input class="form-control" placeholder="Nom d'utilisateur" name="username" autofocus="">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <input class="form-control" placeholder="Mot de passe" name="password" type="password" value="">
+                                                            </div>                            
+                                                            <input class="btn btn-lg btn-success btn-block" type="submit">
+                                                        </fieldset>
+                                                    </form>
+                                                </div>
+                                            <?php } else {
+                                                ?>
+                                                <div class="panel-body col-md-11">
+                                                    <form role="form" method="POST" action="login2.php">
+                                                        <fieldset>
+                                                            <div class="form-group">
+                                                                <input class="form-control" placeholder="Nom d'utilisateur" name="username" autofocus="">
+                                                            </div>
+                                                            <div class="form-group">
+                                                                <input class="form-control" placeholder="Mot de passe" name="password" type="password" value="">
+                                                            </div>                            
+                                                            <input class="btn btn-lg btn-success btn-block" type="submit">
+                                                        </fieldset>
+                                                    </form>
+                                                </div>
+                                            <?php }
+                                            ?>
+                                        </div>
                                     </div>
-                                    <div class="form-group">
-                                        <input class="form-control" placeholder="Mot de passe" name="password" type="password" value="">
-                                    </div>                            
-                                    <input class="btn btn-lg btn-success btn-block" type="submit">
-                                </fieldset>
-                            </form>
+                                </div>
+                            </div>
                         </div>
-                        <?php }
-                        else{ ?>
-                        <div class="panel-body col-md-11">
-                            <form role="form" method="POST" action="login2.php">
-                                <fieldset>
-                                    <div class="form-group">
-                                        <input class="form-control" placeholder="Nom d'utilisateur" name="username" autofocus="">
-                                    </div>
-                                    <div class="form-group">
-                                        <input class="form-control" placeholder="Mot de passe" name="password" type="password" value="">
-                                    </div>                            
-                                    <input class="btn btn-lg btn-success btn-block" type="submit">
-                                </fieldset>
-                            </form>
-                        </div>
-                        <?php }
-                        ?>
                     </div>
                 </div>
             </div>
